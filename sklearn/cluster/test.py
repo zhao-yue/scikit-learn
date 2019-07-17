287d287db855b3ba7785a94a45a05f9142d44d0f
zhao-yue <610714452@qq.com>
2019-07-17 18:34:48 +0100

    Fix setting input size

--- sklearn/cluster/test.py
+++ sklearn/cluster/test.py
@@ -6,14 +6,12 @@ import csv
 from sklearn.cluster.k_means_ import KMeans,k_means
 from mpi4py import MPI
 start_time0 = time.time()
+maxi=1000
+filename='../../../onemillion.csv'
 if(len(sys.argv)>2):
 	filename=sys.argv[2]
-else:
-	filename='../../../onethousand.csv'
-	if(len(sys.argv)>1):
-		maxi=int(sys.argv[1])
-	else:
-		maxi=1000
+if(len(sys.argv)>1):
+	maxi=int(sys.argv[1])
 
 with open(filename,'r') as f:
 	reader = csv.reader(f)
@@ -26,13 +24,19 @@ data=data[0:maxi]
 data=np.array(data).astype(np.float)
 end_time1 = time.time()
 start_time2 = time.time()
-kmeans = KMeans(n_clusters=3, max_iter=300,random_state=35732643,n_init=1,verbose=True,precompute_distances=False,init='k-means++',algorithm='full').fit(data)
+kmeans = KMeans(n_clusters=3, max_iter=600,random_state=35732643,n_init=1,verbose=False,precompute_distances=False,init='k-means++',algorithm='full').fit(data)
 end_time2 = time.time()
 #print(kmeans.labels_)
-print(kmeans.cluster_centers_)
+#print(kmeans.cluster_centers_)
+'''
 if comm.Get_rank()==0:
 	np.savetxt("output12.txt",kmeans.labels_,fmt='%d')
-print ("Open file: %s sec; Preprocess data:%s sec \nExecution time parallel %s sec" % (end_time0-start_time0,end_time1-start_time1,end_time2 - start_time2))
+'''
+comm.Barrier()
+if comm.Get_rank()==0:
+	print ("Open file: %s sec; Preprocess data:%s sec \nExecution time parallel %s sec" % (end_time0-start_time0,end_time1-start_time1,end_time2 - start_time2))
+	print ()
+
 ''' # A very simple test
 X = np.array([[1, 2], [1, 4], [1, 0], [10, 2], [10, 4], [10, 0]])
 kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
